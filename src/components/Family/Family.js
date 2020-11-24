d075c239abe302f97712798e3dd07fe1f9d7fbcf
Andrew Smirnov <sobachylie@gmail.com>
2020-11-24 15:40:03 +0300

    family import fix

--- src/components/Family/Family.js
+++ src/components/Family/Family.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { makeStyles } from "@material-ui/core/styles";
-import { Button } from "@material-ui/core"
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import { 
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Paper
+} from "@material-ui/core"
 import allSpendingActions from "../../actions/spendingActions";
 import SimpleModal from "../Modal/Modal";
-import TextField from "@material-ui/core/TextField";
 import Alert from '@material-ui/lab/Alert';
 
 
